0e8cfecc9fd5e4afc8e209dc04e799ed3102369e
uziel78 <rune.husa78@gmail.com>
2024-02-18 14:34:22 +0100

    Add UserService and user details view

--- databaseProject/routes/users.js
+++ databaseProject/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('../models');
+var UserService = require('../services/UserService');
+var userService = new UserService(db);
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:userId', async function (req, res, next) {
+  const user = await userService.getOne(req.params.userId);
+  res.render('userDetails', { user: user });
 });
 
 module.exports = router;
