e03dc2b8a26efd1805ab60434d11ff6fedfa7998
uziel78 <rune.husa78@gmail.com>
2024-02-18 14:21:27 +0100

    Add makeRate function and handle hotel rating

--- databaseProject/routes/hotels.js
+++ databaseProject/routes/hotels.js
@@ -13,6 +13,11 @@ router.get('/', async function (req, res, next) {
   res.render('hotels', { title: 'Hotels', hotels: hotels });
 });
 
+router.get('/:hotelId', async function (req, res, next) {
+  const hotel = await hotelService.getHotelDetails(req.params.hotelId);
+  res.render('hotelDetails', { hotel: hotel });
+});
+
 router.post('/', jsonParser, async function (req, res, next) {
   let Name = req.body.Name;
   let Location = req.body.Location;
@@ -23,6 +28,13 @@ router.post('/', jsonParser, async function (req, res, next) {
 router.delete('/', jsonParser, async function (req, res, next) {
   let id = req.body.id;
   await hotelService.deleteHotel(id);
+  res.end();
+});
+
+// handle for rate of hotel
+router.post('/:hotelId/rate', jsonParser, async function (req, res, next) {
+  let value = req.body.Value;
+  await hotelService.makeARate(1, req.params.hotelId, value);
   res.end();
 });
 
